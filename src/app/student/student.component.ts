b272d660abe2b8520958289dbcd1429ec77cc929
frank <frank77inf@gmail.com>
2021-10-18 17:11:41 -0400

    show students

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from './Student';
+import { StudentService } from './student.service';
+
 
 @Component({
   selector: 'app-student',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-
-  constructor() { }
+  columnsName: string[] = ['Id', '', 'Currency', 'Status', 'Options'];
+  studentList: Student[] = [];
+  noFound = false;
+  private indice = 0;
+  constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
+    this.studentService.getStudents().subscribe((data:any) => {
+      console.log(data);
+      this.studentList = data;
+    })
   }
 
+  delete(stundeId:number):void{
+    console.log(stundeId);
+  }
+  
 }
